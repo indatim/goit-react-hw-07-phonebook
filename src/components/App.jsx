99647d53228b26ab2e725a55dac1724d7647904e
atimchik <indatim@gmail.com>
2023-06-13 23:04:20 +0300

    goit-react-hw-07-phonebook
    add Loader + create mockapi.io (endpoint: contacts)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import { Container, PhonebookHeader, ContactsHeader } from './App.styled';
 
-import { FaPhoneSquareAlt , FaUser } from "react-icons/fa";
+import { FaPhoneSquareAlt, FaUser } from "react-icons/fa";
+import { Loader } from './Loader/Loader';
 
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -9,12 +10,17 @@ import { Filter } from './ContactFilter/ContactFilter';
 export function App() {
 
 return (
-      <Container>
-        <PhonebookHeader><FaPhoneSquareAlt style={{marginRight: '5'}}/> Phonebook</PhonebookHeader>
-        <ContactForm/>
-        <ContactsHeader><FaUser style={{marginRight: '5'}}/> Contacts</ContactsHeader>
-        <Filter/>
-        <ContactList/>
-      </Container>
-    );
+  <Container>
+    <PhonebookHeader>
+      <FaPhoneSquareAlt style={{ marginRight: '5' }} /> Phonebook
+    </PhonebookHeader>
+    <ContactForm />
+    <ContactsHeader>
+      <FaUser style={{ marginRight: '5' }} /> Contacts
+    </ContactsHeader>
+    <Filter />
+    {/* {isLoading && !error && <Loader/>} */}
+    <ContactList />
+  </Container>
+);
   }
